a90374ef6fb2a86401e3639258594e39a32956af
dilshanSankalpa <dilshansankalpa98@gmail.com>
2021-08-02 21:39:48 +0530

    changed qu1

--- qu1/qu1.scala
+++ qu1/qu1.scala
@@ -1,15 +1,16 @@
 object qu1 extends App{
 
 
-   def isPrime(n : Int, m : Int = 2 , count : Int = 0): Boolean = {
-        if(n == m && count == 0) true;
-        if(count > 0) false;
-        if(n%m == 0) isPrime(n,m+1,count+1);
-        else isPrime(n,m+1,count);
-
-   }
+   def isPrime(i: Int): Boolean =
+    if (i <= 1)
+        false
+    else if (i == 2)
+        true
+    else
+        !(2 until i).exists(n => i % n == 0)
 
     println(isPrime(8));
     println(isPrime(5));
+
 
 }
